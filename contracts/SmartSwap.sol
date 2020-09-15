f19155f31986bc0bfcb79318d2cdca529217b090
张乐平 <holynull@126.com>
2020-09-15 15:19:24 +0800

    migrate uniswap core

--- contracts/SmartSwap.sol
+++ contracts/SmartSwap.sol
@@ -1,12 +1,20 @@
 pragma solidity ^0.6.0;
 
-import "./lib/BEP20.sol";
+import "./BEP20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmartSwap is BEP20 {
+contract SmartSwap is BEP20, Ownable {
     using SafeMath for uint256;
 
     constructor() public BEP20("Smart Swap", "SSWAP") {
         _mint(msg.sender, uint256(100000000).mul(10**18));
+        transferOwnership(msg.sender);
     }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {}
 }
--- contracts/interfaces/IBEP20.sol
+++ contracts/interfaces/IBEP20.sol
@@ -1,15 +1,25 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.6.0;
 
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP.
- */
 interface IBEP20 {
     /**
      * @dev Returns the amount of tokens in existence.
      */
     function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the token decimals.
+     */
+    function decimals() external view returns (uint8);
+
+    /**
+     * @dev Returns the token symbol.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the token name.
+     */
+    function name() external view returns (string memory);
 
     /**
      * @dev Returns the amount of tokens owned by `account`.
@@ -32,7 +42,7 @@ interface IBEP20 {
      *
      * This value changes when {approve} or {transferFrom} are called.
      */
-    function allowance(address owner, address spender) external view returns (uint256);
+    function allowance(address _owner, address spender) external view returns (uint256);
 
     /**
      * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
